147cdc75946520c3dee5ae35cf4ee3bbbc457d19
Menan Vadivel <menan_vadivelpillai@intuit.com>
2018-11-15 17:29:08 -0500

    Trying to move the logic around

--- WebFramework/WebView.swift
+++ WebFramework/WebView.swift
@@ -11,15 +11,96 @@ import WebKit
 
 class WebView: NSObject {
 
-    var webView: WKWebView!
+    
+    static let shared = WebView()
+    
+    var wkWebView = WKWebView()
     let userContentController = WKUserContentController()
     
-    convenience override init() {
-        self.init(with: WKWebView())
+    var delegate: UIViewControllerDelegate?
+    
+    var path = ""
+    
+    override init() {
+        super.init()
+        let config = WKWebViewConfiguration()
+        config.userContentController = userContentController
+        
+        self.wkWebView = WKWebView(frame: UIScreen.main.bounds, configuration: config)
+        
+        userContentController.add(self, name: "pushToNext")
+        userContentController.add(self, name: "dismissView")
+        userContentController.add(self, name: "presentView")
+        userContentController.add(self, name: "setNavTitle")
+        userContentController.add(self, name: "toggleNavBar")
+        userContentController.add(self, name: "setNavBarBackground")
+        userContentController.add(self, name: "setTabBarTint")
+        userContentController.add(self, name: "setTitleFont")
+        userContentController.add(self, name: "toggleScrolling")
+        
+        wkWebView.scrollView.isScrollEnabled = false
+        wkWebView.navigationDelegate = self
+        print("Loading: index.html#!\(path)")
+        let url = Bundle.main.url(forResource: "index", withExtension: "html")!
+        wkWebView.loadFileURL(url, allowingReadAccessTo: url)
+        let request = URLRequest(url: url)
+        wkWebView.load(request)
     }
     
-    init(with webView: WKWebView) {
-        self.webView = webView
-    }
+}
+protocol UIViewControllerDelegate {
+    func pushViewController(path: String?)
+    func presentViewController(path: String?)
+    func dismissViewController()
+    func setNavigationBar(title: String?)
     
+    
+    func doneLoading(withWebView: WKWebView)
+}
+
+
+extension WebView: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Finished navigation b.")
+        delegate?.doneLoading(withWebView: self.wkWebView)
+    }
+}
+
+
+extension WebView: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("Executing: \(message.name)")
+        switch message.name {
+        case "pushToNext":
+            delegate?.pushViewController(path: message.body as? String)
+            break
+        case "dismissView":
+            delegate?.dismissViewController()
+            break
+        case "presentView":
+            delegate?.presentViewController(path: message.body as? String)
+            break
+        case "setNavTitle":
+            delegate?.setNavigationBar(title: message.body as? String)
+            break
+//        case "toggleNavBar":
+//            toggleNavBar(message.body)
+//            break
+//        case "setNavBarBackground":
+//            setNavBarBackground(message.body)
+//            break
+//        case "setTabBarTint":
+//            setTabBarTint(message.body)
+//            break
+//        case "setTitleFont":
+//            setTitleFont(message.body)
+//            break
+//        case "toggleScrolling":
+//            toggleScrolling(message.body)
+//            break
+        default:
+            print("Default Caught.")
+//            delegate?.pushViewController(path: message.body as? String)
+        }
+    }
 }
--- WebFramework/WebViewController.swift
+++ WebFramework/WebViewController.swift
@@ -10,209 +10,239 @@ import UIKit
 import WebKit
 
 class WebViewController: UIViewController {
-
-    var webView: WKWebView!
-    let userContentController = WKUserContentController()
     
     var path = ""
     
-    convenience init() {
-        self.init(with: WKWebView())
-    }
-    
-    init(with webView: WKWebView, and path: String = "") {
-        self.webView = webView
-        self.path = path
+    init() {
         super.init(nibName: nil, bundle: nil)
     }
     
+    init(path: String?) {
+        self.path = path ?? ""
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.white
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        WebView.shared.delegate = self
+        self.view = WebView.shared.wkWebView
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        DispatchQueue.main.async { [unowned self] in
+            
+            if let image = self.screenshot() {
+                let imageView = UIImageView(frame: self.view.frame)
+                imageView.image = image
+                self.view = imageView
+                return
+            }
+            self.view = nil
+        }
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    func screenshot() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, true, UIScreen.main.scale)
+        guard let currentContext = UIGraphicsGetCurrentContext() else { return nil }
+        view.layer.render(in: currentContext)
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else {
+            UIGraphicsEndImageContext()
+            return nil
+        }
         
-        let config = WKWebViewConfiguration()
-        config.userContentController = userContentController
-        
-        self.webView = WKWebView(frame: self.view.bounds, configuration: config)
-        userContentController.add(self, name: "pushToNext")
-        userContentController.add(self, name: "dismissView")
-        userContentController.add(self, name: "presentView")
-        userContentController.add(self, name: "setNavTitle")
-        userContentController.add(self, name: "toggleNavBar")
-        userContentController.add(self, name: "setNavBarBackground")
-        userContentController.add(self, name: "setTabBarTint")
-        userContentController.add(self, name: "setTitleFont")
-        userContentController.add(self, name: "toggleScrolling")
-        
-        
-        setNavTitle("WebFramework")
-        
-        self.view = self.webView
-        
-        
-//        webView.uiDelegate = self
-//        webView.navigationDelegate = self
-        
-        
-        webView.scrollView.isScrollEnabled = false
-        
-        print("Loading: index.html#!\(path)")
-        let url = Bundle.main.url(forResource: "index", withExtension: "html")!
-        webView.loadFileURL(url, allowingReadAccessTo: url)
-        let request = URLRequest(url: url)
-        webView.load(request)
+        UIGraphicsEndImageContext()
+        return image
     }
     
-    @objc func pushToNext(_ message: Any) {
-        let viewController = WebViewController(with: webView, and: message as? String ?? "")
-        
+}
+
+
+extension WebViewController: UIViewControllerDelegate {
+    func pushViewController(path: String?) {
+        let viewController = WebViewController(path: path)
         self.navigationController?.pushViewController(viewController, animated: true)
     }
-    @objc func presentView(_ message:  Any) {
-        
-        let viewController = UINavigationController(rootViewController: WebViewController(with: webView))
-        
-        self.present(viewController, animated: true) {
-            print("Its done.")
-        }
+    func presentViewController(path: String?) {
+        let viewController = WebViewController(path: path)
+        self.navigationController?.present(viewController, animated: true, completion: {
+            self.view = nil
+            viewController.view = WebView.shared.wkWebView
+            print("its Done")
+        })
     }
-    @objc func dismissView(_ message:  Any) {
+    func dismissViewController() {
         self.dismiss(animated: true) {
             print("Its done.")
         }
     }
     
-    @objc func setNavTitle(_ message: Any) {
-        
+    func setNavigationBar(title: String?) {
         var vc: UIViewController!
-        if parent is TabBarViewController {
-            vc = parent
+        if self.parent is TabBarViewController {
+            vc = self.parent
         }
         else {
             vc = self
         }
-        vc.title = message as? String
-        
-//        vc.navigationController?.navigationBar.prefersLargeTitles = true
-
+        vc.title = title
     }
     
-    @objc func setTitleFont(_ message: Any) {
-        
-        if let fontData = message as? [String: Any],
-            let fontName = fontData["fontName"] as? String,
-            let fontSize = fontData["fontSize"] as? Float,
-            let bundle = Bundle(identifier: "com.owl-home-inc.WebFramework"),
-            let url = bundle.url(forResource:fontName, withExtension: "ttf") {
-            do {
-                try UIFont.register(from: url)
-                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: fontName, size: CGFloat(fontSize))!]
-            } catch {
-                print(error)
-            }
-        }
-        
-    }
-    
-    @objc func setTitleColor(_ message: Any) {
-        if let color = message as? String {
-            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: hexStringToUIColor(hex: color)]
-        }
+    func doneLoading(withWebView: WKWebView) {
+        self.view = withWebView
     }
     
     
-    @objc func toggleNavBar(_ message: Any) {
-        guard let navController = self.navigationController else { return }
-        
-        if navController.isNavigationBarHidden {
-            navController.setNavigationBarHidden(false, animated: true)
-        }
-        else {
-            navController.setNavigationBarHidden(true, animated: true)
-        }
-    }
-    
-    
-    @objc func toggleScrolling(_ message: Any) {
-        webView.scrollView.isScrollEnabled = !webView.scrollView.isScrollEnabled
-    }
-    
-    @objc func toggleSidebar(_ message: Any) {
-        //show or hide left bar button item
-        var vc: UIViewController!
-        if parent is TabBarViewController {
-            vc = parent
-        }
-        else {
-            vc = self
-        }
-        
-        if vc.navigationItem.leftBarButtonItem == nil {
-            let menu = UIBarButtonItem(image: UIImage(named: "menu-thin"), style: .plain, target: self, action: #selector(sidebarToggled))
-            vc.navigationItem.leftBarButtonItem  = menu
-        }
-        else {
-            vc.navigationItem.leftBarButtonItem = nil
-        }
-    }
-    
-    @objc func setNavBarBackground(_ message: Any) {
-//        if let imageName = message as? String {
-//            if let image = UIImage(named: "Image-1") {
-//                self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
+//    @objc func pushToNext(_ message: Any) {
+//        let viewController = WebViewController(with: self, and: message as? String ?? "")
+//
+//        delegate.navigationController?.pushViewController(viewController, animated: true)
+//    }
+//    @objc func presentView(_ message:  Any) {
+//
+//        let viewController = UINavigationController(rootViewController: WebViewController(with: self))
+//
+//        delegate.present(viewController, animated: true) {
+//            print("Its done.")
+//        }
+//    }
+//    @objc func dismissView(_ message:  Any) {
+//        delegate.dismiss(animated: true) {
+//            print("Its done.")
+//        }
+//    }
+//
+//    @objc func setNavTitle(_ message: Any) {
+//
+//        var vc: UIViewController!
+//        if delegate.parent is TabBarViewController {
+//            vc = delegate.parent
+//        }
+//        else {
+//            vc = delegate
+//        }
+//        vc.title = message as? String
+//
+//        //        vc.navigationController?.navigationBar.prefersLargeTitles = true
+//
+//    }
+//
+//    @objc func setTitleFont(_ message: Any) {
+//
+//        if let fontData = message as? [String: Any],
+//            let fontName = fontData["fontName"] as? String,
+//            let fontSize = fontData["fontSize"] as? Float,
+//            let bundle = Bundle(identifier: "com.owl-home-inc.WebFramework"),
+//            let url = bundle.url(forResource:fontName, withExtension: "ttf") {
+//            do {
+//                try UIFont.register(from: url)
+//                delegate.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: fontName, size: CGFloat(fontSize))!]
+//            } catch {
+//                print(error)
+//            }
+//        }
+//
+//    }
+//
+//    @objc func setTitleColor(_ message: Any) {
+//        if let color = message as? String {
+//            delegate.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: hexStringToUIColor(hex: color)]
+//        }
+//    }
+//
+//
+//    @objc func toggleNavBar(_ message: Any) {
+//        guard let navController = delegate.navigationController else { return }
+//
+//        if navController.isNavigationBarHidden {
+//            navController.setNavigationBarHidden(false, animated: true)
+//        }
+//        else {
+//            navController.setNavigationBarHidden(true, animated: true)
+//        }
+//    }
+//
+//
+//    @objc func toggleScrolling(_ message: Any) {
+//        wkWebView.scrollView.isScrollEnabled = !wkWebView.scrollView.isScrollEnabled
+//    }
+//
+//    @objc func toggleSidebar(_ message: Any) {
+//        //show or hide left bar button item
+//        var vc: UIViewController!
+//        if delegate.parent is TabBarViewController {
+//            vc = delegate.parent
+//        }
+//        else {
+//            vc = delegate
+//        }
+//
+//        if vc.navigationItem.leftBarButtonItem == nil {
+//            let menu = UIBarButtonItem(image: UIImage(named: "menu-thin"), style: .plain, target: self, action: #selector(sidebarToggled))
+//            vc.navigationItem.leftBarButtonItem  = menu
+//        }
+//        else {
+//            vc.navigationItem.leftBarButtonItem = nil
+//        }
+//    }
+//
+//    @objc func setNavBarBackground(_ message: Any) {
+//        //        if let imageName = message as? String {
+//        //            if let image = UIImage(named: "Image-1") {
+//        //                self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
+//        //            }
+//        //            else {
+//        //                print("Image not found: \(message)")
+//        //            }
+//        //        }
+//        //        else {
+//        //            print("Invalid image path.")
+//        //        }
+//        //
+//        //        let documentDirectory = FileManager.SearchPathDirectory.documentDirectory
+//        //        let userDomainMask    = FileManager.SearchPathDomainMask.userDomainMask
+//        //        let paths             = NSSearchPathForDirectoriesInDomains(documentDirectory, userDomainMask, true)
+//        //        if let dirPath        = paths.first
+//        //        {
+//        //            let imageURL = URL(fileURLWithPath: dirPath).appendingPathComponent("Image-1.png")
+//        //            if let image    = UIImage(contentsOfFile: imageURL.path) {
+//        //                self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
+//        //            }
+//        //            else {
+//        //                print("Image not found.")
+//        //            }
+//        //            // Do whatever you want with the image
+//        //        }
+//
+//        if let imagePath = Bundle.main.path(forResource:"Image-1", ofType: "png", inDirectory: "Images") {
+//            print("Image URL: \(imagePath)")
+//            if let image = UIImage(contentsOfFile: imagePath) {
+//                print("Image: \(image)")
+//                delegate.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
 //            }
 //            else {
-//                print("Image not found: \(message)")
+//                print("Can't open image at: \(imagePath).")
 //            }
 //        }
 //        else {
-//            print("Invalid image path.")
+//            print("Image not found.")
 //        }
+//    }
 //
-//        let documentDirectory = FileManager.SearchPathDirectory.documentDirectory
-//        let userDomainMask    = FileManager.SearchPathDomainMask.userDomainMask
-//        let paths             = NSSearchPathForDirectoriesInDomains(documentDirectory, userDomainMask, true)
-//        if let dirPath        = paths.first
-//        {
-//            let imageURL = URL(fileURLWithPath: dirPath).appendingPathComponent("Image-1.png")
-//            if let image    = UIImage(contentsOfFile: imageURL.path) {
-//                self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
-//            }
-//            else {
-//                print("Image not found.")
-//            }
-//            // Do whatever you want with the image
+//    @objc func setTabBarTint(_ message: Any) {
+//        if let color = message as? String {
+//            delegate.tabBarController?.tabBar.tintColor = hexStringToUIColor(hex: color)
 //        }
-        
-        if let imagePath = Bundle.main.path(forResource:"Image-1", ofType: "png", inDirectory: "Images") {
-            print("Image URL: \(imagePath)")
-            if let image = UIImage(contentsOfFile: imagePath) {
-                print("Image: \(image)")
-                self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
-            }
-            else {
-                print("Can't open image at: \(imagePath).")
-            }
-        }
-        else {
-            print("Image not found.")
-        }
-    }
-    
-    @objc func setTabBarTint(_ message: Any) {
-        if let color = message as? String {
-            self.tabBarController?.tabBar.tintColor = hexStringToUIColor(hex: color)
-        }
-    }
-    
-    @objc func sidebarToggled(_ message: Any) {
-        
-        
-    }
+//    }
+//
+//    @objc func sidebarToggled(_ message: Any) {
+//
+//
+//    }
     
     func hexStringToUIColor (hex:String) -> UIColor {
         var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
@@ -234,44 +264,6 @@ class WebViewController: UIViewController {
             blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
             alpha: CGFloat(1.0)
         )
-    }
-}
-
-
-extension WebViewController: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print("Executing: \(message.name)")
-        switch message.name {
-        case "pushToNext":
-            pushToNext(message.body)
-            break
-        case "dismissView":
-            dismissView(message.body)
-            break
-        case "presentView":
-            presentView(message.body)
-            break
-        case "setNavTitle":
-            setNavTitle(message.body)
-            break
-        case "toggleNavBar":
-            toggleNavBar(message.body)
-            break
-        case "setNavBarBackground":
-            setNavBarBackground(message.body)
-            break
-        case "setTabBarTint":
-            setTabBarTint(message.body)
-            break
-        case "setTitleFont":
-            setTitleFont(message.body)
-            break
-        case "toggleScrolling":
-            toggleScrolling(message.body)
-            break
-        default:
-            pushToNext(message.body)
-        }
     }
 }
 
